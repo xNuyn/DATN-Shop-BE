ddcb617452de77092cae4ee1734540724cce6079
nuynt <tranxuannguyen732@gmail.com>
2025-05-30 01:45:08 +0700

    chore: cors

--- config/settings/base.py
+++ config/settings/base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'authentication',
     'cloudinary',
     'cloudinary_storage',
+    'corsheaders',
     'users',
     'products',
     'orders',
@@ -62,6 +63,7 @@ CLOUDINARY_STORAGE = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -69,6 +71,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
 ]
 
 REST_FRAMEWORK = {
